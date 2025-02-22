3dd076e963cf4de92e9b687a51185bee4838a9fa
Marco Meyer-Conde <marco@tcu.ac.jp>
2025-02-22 15:55:57 +0900

    Initial commit

---
+++ recipe/build.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+mkdir build-scripts
+cd build-scripts
+
+cmake $RECIPE_DIR/scripts
+cd ..
+
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+
+make -j$(nproc)
+make install
